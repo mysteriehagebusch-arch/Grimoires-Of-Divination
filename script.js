52abde055c0436e249ed02c310dbc7c370bd5a45
mysteriehagebusch-arch <mysteriehagebusch@gmail.com>
2026-01-19 00:30:43 -0600

    Update script.js
    Signed-off-by: mysteriehagebusch-arch <mysteriehagebusch@gmail.com>

--- script.js
+++ script.js
@@ -172,7 +172,7 @@ setTimeout(() => {
         intro.style.opacity = "0";
         setTimeout(() => intro.remove(), 600);
     }
-}, 125000); // 120s delay + 120s animation 
+}, 125000); // 120s delay + 120s animation + 5s opening after 120s
 
 /* ---------------------------------------------------------
    11. LOGIN GATE LOGIC
